a3b48bc9166fa99aedb27a2d62ea24f43fe9b39e
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-07-06 02:46:01 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import './css/App.css'
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        {/* <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route> */}
+      </Routes>
+    </>
+  )
+}
